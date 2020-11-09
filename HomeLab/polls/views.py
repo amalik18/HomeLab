5038f5c7404484a3cf79242846d1d719e32cb0e7
amalik18 <ali_malik96@yahoo.com>
2020-11-09 01:05:53 -0500

    Django tutorial

--- HomeLab/polls/views.py
+++ HomeLab/polls/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+
 
 # Create your views here.
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request=request, template_name='polls/index.html', context=context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("The Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request=request, template_name='polls/detail.html', context=context)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}"
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}".format(question_id))
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import setuptools
-rt setuptools
 
 with open("README.md", "r") as fh:
         long_description = fh.read()
